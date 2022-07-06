62d386301c89d3015c1df1291606437fcdd42bdb
francobss <francobornancini2000@gmail.com>
2022-07-05 21:41:26 -0300

    desafio 2

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,6 @@
 
+// desafio complementario 1
+/*
 let cantVueltas = prompt("Ingresar la cantidad de numeros: ")
 
 for (let i = 0; i <= cantVueltas; i++){
@@ -9,3 +11,28 @@ for (let i = 0; i <= cantVueltas; i++){
     suma = numeroAPedir + x
     console.log("resultado final: " + suma)
 }
+*/
+
+// Desafio 2 (clase 4) para proyecto 
+function promediar(cant, div){
+    let promedio = 0
+    if (cant > 0 && div > 0){
+        promedio = cant / div;
+    }
+    return promedio;
+}
+
+
+ // calculando edad promedio de personas ingresadas.
+
+let cantPersonas = Number(prompt('ingrese la cantidad de personas.'))
+let edadPersona;
+let acumEdad = 0;
+
+for(i = 0; i < cantPersonas; i++) {
+    edadPersona = Number(prompt('ingrese la edad de la persona.'));
+    acumEdad = acumEdad + edadPersona;
+}
+
+let promedio = promediar(acumEdad, cantPersonas);
+console.log('El promedio de edad es ' + promedio)
