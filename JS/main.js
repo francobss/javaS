2b5422205bf3c0fd161ae28a4395c26b4eae54b9
francobss <francobornancini2000@gmail.com>
2022-07-11 23:17:26 -0300

    desafio3

--- JS/main.js
+++ JS/main.js
@@ -12,7 +12,8 @@ for (let i = 0; i <= cantVueltas; i++){
     console.log("resultado final: " + suma)
 }
 */
-
+/*
+// =======================================================
 // Desafio 2 (clase 4) para proyecto 
 function promediar(cant, div){
     let promedio = 0
@@ -35,3 +36,56 @@ for(i = 0; i < cantPersonas; i++) {
 
 let promedio = promediar(acumEdad, cantPersonas);
 console.log('El promedio de edad es ' + promedio)
+*/
+
+// =======================================================
+// Desafío: Incorporar Arrays
+class persona{
+    constructor(nombre, edad) {
+        this.nombre = nombre;
+        this.edad = edad;
+        
+    }
+}
+class productos{
+    constructor(ident, precio) {
+        this.ident = ident;
+        this.precio = precio;
+        
+    }
+}
+
+let entrada = parseInt(prompt('Ingrese su opcion.\n 1- cargar clientes.\n 2- cargar productos.\n 3- mostrar arrays.\n 4- Salir'));
+let cantidad;
+const listaNombres = [];
+const listaProductos = [];
+while(entrada != 4){
+    switch(entrada){
+        case 1:
+            cantidad = parseInt(prompt('Ingrese la cantidad de clientes.'));
+            for(i = 1; i <= cantidad; i++){
+                const nombre = prompt('Ingrese el nombre.');
+                const edad = prompt('Ingrese la edad.');
+                listaNombres.push(new persona(nombre, edad))
+            }
+            break;
+        case 2:
+            cantidad = parseInt(prompt('Ingrese la cantidad de productos.'));
+            for(i = 1; i <= cantidad; i++){
+                const prod = prompt('Ingrese el producto.');
+                const valor = prompt('Ingrese el valor.');
+                listaProductos.push(new productos(prod, valor))
+            }
+        case 3:
+            console.log(listaNombres)
+            console.log(listaProductos)
+        default:
+            console.log('Opcion erronea')
+            break;
+    }
+    entrada = parseInt(prompt('Ingrese su opcion.\n 1- cargar clientes.\n 2- cargar productos.\n 3- mostrar arrays.\n 4- Salir'));
+}
+
+if (listaNombres.length === 0 && listaProductos.length === 0){
+    console.log('No has cargado ningun dato')
+}
